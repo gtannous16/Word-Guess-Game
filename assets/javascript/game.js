5c44b5dc8b394039507612ba6a3d63a2e5bbc4cf
Gigi Tannous <gtannous16@gmail.com>
2019-03-16 10:55:10 -0400

    formated everything to look nice and neat

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,30 +1,30 @@
 //declare and initialize array
-var words = ["lumos", 
-"mcgonagall", "alohomora", 
-"hippogriff","quidditch", 
-"durmstrang", "gryffindor", 
-"beauxbatons", "ravenclaw", 
-"quibbler", "hufflepuff", 
-"norbert", "patronus", 
-"hagrid", "hogwarts", 
-"harry",  "dumbledore",
-"hermione", "ron", 
-"gringotts","slytherin",
-"neville", "luna",
-"potions", "dobby",
-"draco", "voldemort",
-"hogsmeade", "diagonalley",
-"hedwig", "ginny",
-"bellatrix", "snape",
-"nagini","madeye",
-"sirius", "weaslytwins",
-"azkaban", "dementor",
-"headboy", "headgirl",
-"prefect", "maraudersmap",
-"umbridge", "horcrux",
-"accio", "riddikulus",
-"muggle"]
-
+var words = ["lumos",
+    "mcgonagall", "alohomora",
+    "hippogriff", "quidditch",
+    "durmstrang", "gryffindor",
+    "beauxbatons", "ravenclaw",
+    "quibbler", "hufflepuff",
+    "norbert", "patronus",
+    "hagrid", "hogwarts",
+    "harry", "dumbledore",
+    "hermione", "ron",
+    "gringotts", "slytherin",
+    "neville", "luna",
+    "potions", "dobby",
+    "draco", "voldemort",
+    "hogsmeade", "diagonalley",
+    "hedwig", "ginny",
+    "bellatrix", "snape",
+    "nagini", "madeye",
+    "sirius", "weaslytwins",
+    "azkaban", "dementor",
+    "headboy", "headgirl",
+    "prefect", "maraudersmap",
+    "umbridge", "horcrux",
+    "accio", "riddikulus",
+    "muggle"
+]
 
 //assigning variabls 
 var randWord = "";
@@ -42,7 +42,7 @@ var guessesRemaining = 15;
 //choose word randomly, seperating each letter &
 //adding underscores in place of each letter
 function Game() {
-    
+
     randWord = words[Math.floor(Math.random() * words.length)];
 
     lettersOfWord = randWord.split("");
@@ -69,20 +69,19 @@ function reset() {
     Game()
 }
 
-
 //variables for audio function
 var hedwig7 = document.getElementById("hedwig7");
 var hedwig8 = document.getElementById("hedwig8");
 var harrypotter = document.getElementById("harrypotter");
 //audio function
 function aud() {
-  //lumos image
+    //lumos image
     if (randWord === words[0]) {
         harrypotter.pause();
         hedwig7.play();
         document.getElementById("image").src = "./assets/images/lumos.gif";
     }
-   //mcgonagall image
+    //mcgonagall image
     else if (randWord === words[1]) {
         harrypotter.pause();
         hedwig7.play();
@@ -118,272 +117,267 @@ function aud() {
         hedwig7.play();
         document.getElementById("image").src = "./assets/images/gryffindor.gif";
     }
-     // image
-     else if (randWord === words[7]) {
+    // image
+    else if (randWord === words[7]) {
         harrypotter.pause();
         hedwig7.play();
         document.getElementById("image").src = "./assets/images/beauxbatons.gif";
     }
-     // image
-     else if (randWord === words[8]) {
+    // image
+    else if (randWord === words[8]) {
         harrypotter.pause();
         hedwig7.play();
         document.getElementById("image").src = "./assets/images/ravenclaw.gif";
     }
-     // image
-     else if (randWord === words[9]) {
+    // image
+    else if (randWord === words[9]) {
         harrypotter.pause();
         hedwig7.play();
         document.getElementById("image").src = "./assets/images/quibbler.gif";
     }
-     // image
-     else if (randWord === words[10]) {
+    // image
+    else if (randWord === words[10]) {
         harrypotter.pause();
         hedwig7.play();
         document.getElementById("image").src = "./assets/images/hufflepuff.gif";
     }
-     // image
-     else if (randWord === words[11]) {
+    // image
+    else if (randWord === words[11]) {
         harrypotter.pause();
         hedwig7.play();
         document.getElementById("image").src = "./assets/images/norbert.gif";
     }
-     // image
-     else if (randWord === words[12]) {
+    // image
+    else if (randWord === words[12]) {
         harrypotter.pause();
         hedwig7.play();
         document.getElementById("image").src = "./assets/images/patronus.gif";
     }
-     // image
-     else if (randWord === words[13]) {
+    // image
+    else if (randWord === words[13]) {
         harrypotter.pause();
         hedwig7.play();
         document.getElementById("image").src = "./assets/images/hagrid.gif";
     }
-     // image
-     else if (randWord === words[14]) {
+    // image
+    else if (randWord === words[14]) {
         harrypotter.pause();
         hedwig7.play();
         document.getElementById("image").src = "./assets/images/hogwarts.gif";
     }
-     // image
-     else if (randWord === words[15]) {
+    // image
+    else if (randWord === words[15]) {
         harrypotter.pause();
         hedwig7.play();
         document.getElementById("image").src = "./assets/images/harrypotter.gif";
     }
-     // image
-     else if (randWord === words[16]) {
+    // image
+    else if (randWord === words[16]) {
         harrypotter.pause();
         hedwig7.play();
         document.getElementById("image").src = "./assets/images/dumbledor.gif";
     }
-     // image
-     else if (randWord === words[17]) {
+    // image
+    else if (randWord === words[17]) {
         harrypotter.pause();
         hedwig7.play();
         document.getElementById("image").src = "./assets/images/hermione.gif";
     }
-     // image
-     else if (randWord === words[18]) {
+    // image
+    else if (randWord === words[18]) {
         harrypotter.pause();
         hedwig7.play();
         document.getElementById("image").src = "./assets/images/ron.gif";
     }
-     // image
-     else if (randWord === words[19]) {
+    // image
+    else if (randWord === words[19]) {
         harrypotter.pause();
         hedwig7.play();
         document.getElementById("image").src = "./assets/images/gringots.gif";
     }
-     // image
-     else if (randWord === words[20]) {
+    // image
+    else if (randWord === words[20]) {
         harrypotter.pause();
         hedwig7.play();
         document.getElementById("image").src = "./assets/images/slytherin.gif";
     }
-     // image
-     else if (randWord === words[21]) {
+    // image
+    else if (randWord === words[21]) {
         harrypotter.pause();
         hedwig7.play();
         document.getElementById("image").src = "./assets/images/neville.gif";
     }
-     // image
-     else if (randWord === words[22]) {
+    // image
+    else if (randWord === words[22]) {
         harrypotter.pause();
         hedwig7.play();
         document.getElementById("image").src = "./assets/images/luna.gif";
     }
-     // image
-     else if (randWord === words[23]) {
+    // image
+    else if (randWord === words[23]) {
         harrypotter.pause();
         hedwig7.play();
         document.getElementById("image").src = "./assets/images/potions.jpeg";
     }
-     // image
-     else if (randWord === words[24]) {
+    // image
+    else if (randWord === words[24]) {
         harrypotter.pause();
         hedwig7.play();
         document.getElementById("image").src = "./assets/images/dobby.gif";
     }
-     // image
-     else if (randWord === words[25]) {
+    // image
+    else if (randWord === words[25]) {
         harrypotter.pause();
         hedwig7.play();
         document.getElementById("image").src = "./assets/images/dracomalfoy.gif";
     }
-     // image
-     else if (randWord === words[26]) {
+    // image
+    else if (randWord === words[26]) {
         harrypotter.pause();
         hedwig7.play();
         document.getElementById("image").src = "./assets/images/voldemort.gif";
     }
-     // image
-     else if (randWord === words[27]) {
+    // image
+    else if (randWord === words[27]) {
         harrypotter.pause();
         hedwig7.play();
         document.getElementById("image").src = "./assets/images/hogsmeade.gif";
     }
-     // image
-     else if (randWord === words[28]) {
+    // image
+    else if (randWord === words[28]) {
         harrypotter.pause();
         hedwig7.play();
         document.getElementById("image").src = "./assets/images/diagonalley.gif";
     }
-     // image
-     else if (randWord === words[29]) {
+    // image
+    else if (randWord === words[29]) {
         harrypotter.pause();
         hedwig7.play();
         document.getElementById("image").src = "./assets/images/hedwig.gif";
     }
-     // image
-     else if (randWord === words[30]) {
+    // image
+    else if (randWord === words[30]) {
         harrypotter.pause();
         hedwig7.play();
         document.getElementById("image").src = "./assets/images/ginny.gif";
     }
-     // image
-     else if (randWord === words[31]) {
+    // image
+    else if (randWord === words[31]) {
         harrypotter.pause();
         hedwig7.play();
         document.getElementById("image").src = "./assets/images/bellatrix.gif";
     }
-     // image
-     else if (randWord === words[32]) {
+    // image
+    else if (randWord === words[32]) {
         harrypotter.pause();
         hedwig7.play();
         document.getElementById("image").src = "./assets/images/snapeapproves.gif";
     }
-     // image
-     else if (randWord === words[33]) {
+    // image
+    else if (randWord === words[33]) {
         harrypotter.pause();
         hedwig7.play();
         document.getElementById("image").src = "./assets/images/nagini.gif";
     }
-     // image
-     else if (randWord === words[34]) {
+    // image
+    else if (randWord === words[34]) {
         harrypotter.pause();
         hedwig7.play();
         document.getElementById("image").src = "./assets/images/madeyemoody.gif";
     }
-     // image
-     else if (randWord === words[35]) {
+    // image
+    else if (randWord === words[35]) {
         harrypotter.pause();
         hedwig7.play();
         document.getElementById("image").src = "./assets/images/sirius.gif";
     }
-     // image
-     else if (randWord === words[36]) {
+    // image
+    else if (randWord === words[36]) {
         harrypotter.pause();
         hedwig7.play();
         document.getElementById("image").src = "./assets/images/twins.gif";
     }
-     // image
-     else if (randWord === words[37]) {
+    // image
+    else if (randWord === words[37]) {
         harrypotter.pause();
         hedwig7.play();
         document.getElementById("image").src = "./assets/images/azkaban.gif";
     }
-     // image
-     else if (randWord === words[38]) {
+    // image
+    else if (randWord === words[38]) {
         harrypotter.pause();
         hedwig7.play();
         document.getElementById("image").src = "./assets/images/dementor.gif";
     }
-     // image
-     else if (randWord === words[39]) {
+    // image
+    else if (randWord === words[39]) {
         harrypotter.pause();
         hedwig7.play();
         document.getElementById("image").src = "./assets/images/headboy.jpeg";
     }
-     // image
-     else if (randWord === words[40]) {
+    // image
+    else if (randWord === words[40]) {
         harrypotter.pause();
         hedwig7.play();
         document.getElementById("image").src = "./assets/images/headgirl.jpeg";
     }
-     // image
-     else if (randWord === words[41]) {
+    // image
+    else if (randWord === words[41]) {
         harrypotter.pause();
         hedwig7.play();
         document.getElementById("image").src = "./assets/images/prefect.gif";
     }
-     // image
-     else if (randWord === words[42]) {
+    // image
+    else if (randWord === words[42]) {
         harrypotter.pause();
         hedwig7.play();
         document.getElementById("image").src = "./assets/images/maraudersmap.gif";
     }
-     // image
-     else if (randWord === words[43]) {
+    // image
+    else if (randWord === words[43]) {
         harrypotter.pause();
         hedwig7.play();
         document.getElementById("image").src = "./assets/images/umbridge.gif";
     }
-     // image
-     else if (randWord === words[44]) {
+    // image
+    else if (randWord === words[44]) {
         harrypotter.pause();
         hedwig7.play();
         document.getElementById("image").src = "./assets/images/horcrux.gif";
     }
-     // image
-     else if (randWord === words[45]) {
+    // image
+    else if (randWord === words[45]) {
         harrypotter.pause();
         hedwig7.play();
         document.getElementById("image").src = "./assets/images/accio.gif";
-    }
-    else if (randWord === words[46]) {
+    } else if (randWord === words[46]) {
         harrypotter.pause();
         hedwig7.play();
         document.getElementById("image").src = "./assets/images/riddikulus.gif";
-    }
-    else if (randWord === words[47]) {
+    } else if (randWord === words[47]) {
         harrypotter.pause();
         hedwig7.play();
         document.getElementById("image").src = "./assets/images/muggle.gif";
     }
 };
 
-
 //this checks each letter guessed by player to see if its right or wrong
 function checkLetters(letter) {
     var letterInWord = false;
-    
+
     for (var i = 0; i < underScores; i++) {
         if (randWord[i] == letter) {
             letterInWord = true;
         }
     }
-    
+
     if (letterInWord) {
         for (var i = 0; i < underScores; i++) {
             if (randWord[i] == letter) {
                 underScoresCorrect[i] = letter;
             }
         }
-    }
-   
-    else {
+    } else {
         wrongGuess.push(letter);
         guessesRemaining--;
     }
@@ -393,8 +387,8 @@ function checkLetters(letter) {
 //completing the game
 function complete() {
     console.log("wins:" + wins + "| losses:" + losses + "| guesses left:" + guessesRemaining)
-//if all letters are guessed correctly then game resets and adds to winstracker
-//if there are no remaining guesses left game resets and adds to losstracker
+        //if all letters are guessed correctly then game resets and adds to winstracker
+        //if there are no remaining guesses left game resets and adds to losstracker
     if (lettersOfWord.toString() == underScoresCorrect.toString()) {
         wins++;
         aud()
@@ -408,18 +402,16 @@ function complete() {
         document.getElementById("image").src = "./assets/images/tryagain.jpg"
         document.getElementById("losstracker").innerHTML = " " + losses;
     }
-    
+
     document.getElementById("currentword").innerHTML = "  " + underScoresCorrect.join(" ");
     document.getElementById("guessesremaining").innerHTML = " " + guessesRemaining;
 }
 
-
-
 //start game function
 Game()
-document.onkeyup = function (event) {
+document.onkeyup = function(event) {
     var guesses = String.fromCharCode(event.keyCode).toLowerCase();
-    checkLetters(guesses); 
+    checkLetters(guesses);
     complete();
     console.log(guesses);
 
